1e1b38e2929eee99b2c7cea37586fcf22c1c6310
Dmitry-Oksentyuk <reklama@gmail.com>
2017-02-06 17:01:25 +0300

    mailer photo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,8 +48,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
       port: '587',
-      user_name: 'reklama512', # не используйте для тестов свои реальные ящики
-      password: 'maz4370maz', # не храните здесь пароль!
+      user_name: 'ваш юзернейм', # не используйте для тестов свои реальные ящики
+      password: 'ваш пароль', # не храните здесь пароль!
       authentication: 'plain',
       enable_starttls_auto: true
   }
